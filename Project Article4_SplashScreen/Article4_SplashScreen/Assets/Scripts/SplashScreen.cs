d780997db027f023851f9ba4a017ad38240d5d0b
Budi Fajri Arzi <Budi Fajri Arzi>
2016-01-06 14:59:22 +0700

    nambah project splash screen

---
+++ Project Article4_SplashScreen/Article4_SplashScreen/Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreen : MonoBehaviour {
+
+	Color colorStart = new Color(0,0,0,0);
+	Color colorEnd = new Color(1,1,1,1);
+	Renderer rend;
+	public float speed = 0.33f;
+	public float wait = 2.5f;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer> ();
+		rend.material.color = colorStart;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rend.material.color != colorEnd)
+			rend.material.color = Color.Lerp (colorStart, colorEnd, Time.time * speed);
+		else
+			StartCoroutine(LoadMainMenu());
+	}
+
+	IEnumerator LoadMainMenu()
+	{
+		yield return new WaitForSeconds (wait);
+		Application.LoadLevel("mainmenu");
+	}
+
+}
